e1df6d9eea569ea7bc1ef4e14ceb8382ea015068
ericakn <erica.k.nishimura@gmail.com>
2025-03-14 14:52:58 -0300

    feat: add try catch to deal with errors

--- src/foreign_coins/conversion_api.py
+++ src/foreign_coins/conversion_api.py
@@ -6,49 +6,56 @@ import argparse
 logger = logging.getLogger(__name__)
 
 def quotation(option=1):
-
-    # Choose which conversion you would like
-    if option == 1:
-        coin = 'EURBRL'
-    elif option == 2:
-        coin = 'USDBRL'
-    elif option == 3:
-        coin = 'BTCBRL'
-    else:
-        return 'Invalid option!'
-
-
-# API's URL
-    url = 'https://economia.awesomeapi.com.br/json/last/'+ coin[0:3] +'-'+ coin[3:6]
+    try:
+        # Choose which conversion you would like
+        if option == 1:
+            coin = 'EURBRL'
+        elif option == 2:
+            coin = 'USDBRL'
+        elif option == 3:
+            coin = 'BTCBRL'
+        else:
+            return 'Invalid option!'
 
 
-# Request information
-    quotation = requests.get(url).content
+        # API's URL
+        url = 'https://economia.awesomeapi.com.br/json/last/'+ coin[0:3] +'-'+ coin[3:6]
 
-# Extract info
-    dic = json.loads(quotation)
 
-    return coin, dic[coin]
+        # Request information
+        quotation = requests.get(url).content
+
+        # Extract info
+        dic = json.loads(quotation)
+
+        return coin, dic[coin]
+    except Exception as e:
+        logger.error("Error in quotation function: %s", e)
+        return None, str(e)
 
 def main():
     parser = argparse.ArgumentParser(description='Currency quotation')
     parser.add_argument('--option', type=int, required=True, help='Options: (1: EURBRL, 2: USDBRL, 3: BTCBRL)')
+    try:
+        # Testing some errors:
+        # raise Exception("This is a test exception")
+        # a =1 + '1'
+        args = parser.parse_args()
+        coin, result = quotation(args.option) 
 
-    args = parser.parse_args()
-    coin, result = quotation(args.option) 
-     
-    if isinstance(result, str): # check if there is an error message
-        print("error!")
-        print(result)
-    else:
-        date_hour = result["create_date"]
-        message = (f'{date_hour[8:10]}/{date_hour[5:7]}/{date_hour[0:4]} {date_hour[10:19]} - Preço atual ({coin}): {result["bid"]} Brazilians Reais\n')
-        with open("quotation_api.txt", "a") as file:
-            logging.basicConfig(filename='quotation_logs_api.log', level=logging.INFO)
-            logger.info('Started')
-            file.write(message)
-            logger.info("Quotation for %s successfully saved: %s", coin, message.strip())
-        print(message)
-
+        if isinstance(result, str): # check if there is an error message
+            print("error!")
+            print(result)
+        else:
+            date_hour = result["create_date"]
+            message = (f'{date_hour[8:10]}/{date_hour[5:7]}/{date_hour[0:4]} {date_hour[10:19]} - Preço atual ({coin}): {result["bid"]} Brazilians Reais\n')
+            with open("quotation_api.txt", "a") as file:
+                logging.basicConfig(filename='quotation_logs_api.log', level=logging.INFO)
+                logger.info('Started')
+                file.write(message)
+                logger.info("Quotation for %s successfully saved: %s", coin, message.strip())
+            print(message)
+    except Exception as e:
+        logger.error("ERror in main function: %s", e)
 if __name__ == '__main__':
     main()
